fd51aa758d62f4a1cedb142ee20515571bd3f0f3
Era <azanaera691@gmail.com>
2019-04-03 18:15:42 +0800

    initial commit

---
+++ SnapshotForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Snapshot_Maker
+{
+    partial class SnapshotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.sfdPicture = new System.Windows.Forms.SaveFileDialog();
+            this.ofdPicture = new System.Windows.Forms.OpenFileDialog();
+            this.picCropped = new System.Windows.Forms.PictureBox();
+            this.Reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCropped)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(13, 12);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 28);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "&Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Snapshot time:";
+            // 
+            // timeBox
+            // 
+            this.timeBox.Location = new System.Drawing.Point(253, 15);
+            this.timeBox.Margin = new System.Windows.Forms.Padding(4);
+            this.timeBox.Name = "timeBox";
+            this.timeBox.ReadOnly = true;
+            this.timeBox.Size = new System.Drawing.Size(199, 22);
+            this.timeBox.TabIndex = 3;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "JPEG images (*.jpg)|*.jpg|PNG images (*.png)|*.png|BMP images (*.bmp)|*.bmp";
+            this.saveFileDialog.Title = "Save snapshot";
+            // 
+            // ofdPicture
+            // 
+            this.ofdPicture.FileName = "openFileDialog1";
+            // 
+            // picCropped
+            // 
+            this.picCropped.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.picCropped.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picCropped.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picCropped.Location = new System.Drawing.Point(13, 48);
+            this.picCropped.Margin = new System.Windows.Forms.Padding(4);
+            this.picCropped.Name = "picCropped";
+            this.picCropped.Size = new System.Drawing.Size(579, 363);
+            this.picCropped.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCropped.TabIndex = 4;
+            this.picCropped.TabStop = false;
+            this.picCropped.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picCropped_MouseDown);
+            this.picCropped.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picCropped_MouseMove);
+            this.picCropped.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picCropped_MouseUp);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(650, 12);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(123, 30);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "button1";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.mnuPictureReset_Click);
+            // 
+            // SnapshotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 423);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.picCropped);
+            this.Controls.Add(this.timeBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SnapshotForm";
+            this.Text = "Snapshot";
+            ((System.ComponentModel.ISupportInitialize)(this.picCropped)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox timeBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.SaveFileDialog sfdPicture;
+        private System.Windows.Forms.OpenFileDialog ofdPicture;
+        private System.Windows.Forms.PictureBox picCropped;
+        private System.Windows.Forms.Button Reset;
+    }
+}
---
+++ SnapshotForm.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Snapshot_Maker
+{
+    public partial class SnapshotForm : Form
+    {
+        // The original image.
+        private Bitmap OriginalImage = null;
+
+        // The currently cropped image.
+        private Bitmap CroppedImage;
+
+        // The cropped image with the selection rectangle.
+        private Bitmap DisplayImage;
+        private Graphics DisplayGraphics;
+
+        public SnapshotForm( )
+        {
+            InitializeComponent( );
+        }
+
+        public void SetImage( Bitmap bitmap )
+        {
+                timeBox.Text = DateTime.Now.ToLongTimeString( );
+
+                //Bitmap old = (Bitmap) pictureBox.Image;
+                //pictureBox.Image = bitmap;
+                picCropped.Image = bitmap;
+
+                OriginalImage = bitmap;
+                CroppedImage = (Bitmap)bitmap.Clone();
+                DisplayImage = (Bitmap)CroppedImage.Clone();
+                DisplayGraphics = Graphics.FromImage(DisplayImage);
+
+                picCropped.Image = DisplayImage;
+                picCropped.Visible = true;
+            //if ( old != null )
+            //{
+            //    old.Dispose( );
+            //}
+        }
+        
+
+        private void saveButton_Click( object sender, EventArgs e )
+        {
+            if ( saveFileDialog.ShowDialog( ) == DialogResult.OK )
+            {
+                string ext = Path.GetExtension( saveFileDialog.FileName ).ToLower( );
+                ImageFormat format = ImageFormat.Jpeg;
+
+                if ( ext == ".bmp" )
+                {
+                    format = ImageFormat.Bmp;
+                }
+                else if ( ext == ".png" )
+                {
+                    format = ImageFormat.Png;
+                }
+
+                try
+                {
+                    lock ( this )
+                    {
+                        Bitmap image = (Bitmap) picCropped.Image;
+                        image.Save(saveFileDialog.FileName, format);
+                    }
+                }
+                catch ( Exception ex )
+                {
+                    MessageBox.Show( "Failed saving the snapshot.\n" + ex.Message,
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        // Open a file.
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            if (ofdPicture.ShowDialog() == DialogResult.OK)
+            {
+                OriginalImage = LoadBitmapUnlocked(ofdPicture.FileName);
+                CroppedImage = OriginalImage.Clone() as Bitmap;
+                DisplayImage = CroppedImage.Clone() as Bitmap;
+                DisplayGraphics = Graphics.FromImage(DisplayImage);
+
+                picCropped.Image = DisplayImage;
+                picCropped.Visible = true;
+            }
+        }
+
+        // Let the user select an area.
+        private bool Drawing = false;
+        private Point StartPoint, EndPoint;
+        private void picCropped_MouseDown(object sender, MouseEventArgs e)
+        {
+            Drawing = true;
+            StartPoint = e.Location;
+
+            // Draw the area selected.
+            DrawSelectionBox(e.Location);
+        }
+
+        private void picCropped_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Drawing) return;
+
+            // Draw the area selected.
+            DrawSelectionBox(e.Location);
+        }
+
+        private void picCropped_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!Drawing) return;
+            Drawing = false;
+
+            // Crop.
+            // Get the selected area's dimensions.
+            int x = Math.Min(StartPoint.X, EndPoint.X);
+            int y = Math.Min(StartPoint.Y, EndPoint.Y);
+            int width = Math.Abs(StartPoint.X - EndPoint.X);
+            int height = Math.Abs(StartPoint.Y - EndPoint.Y);
+            Rectangle source_rect = new Rectangle(x, y, width, height);
+            Rectangle dest_rect = new Rectangle(0, 0, width, height);
+
+            // Copy that part of the image to a new bitmap.
+            DisplayImage = new Bitmap(width, height);
+            DisplayGraphics = Graphics.FromImage(DisplayImage);
+            DisplayGraphics.DrawImage(CroppedImage, dest_rect, source_rect, GraphicsUnit.Pixel);
+
+            // Display the new bitmap.
+            CroppedImage = DisplayImage;
+            DisplayImage = CroppedImage.Clone() as Bitmap;
+            DisplayGraphics = Graphics.FromImage(DisplayImage);
+            picCropped.Image = DisplayImage;
+            picCropped.Refresh();
+        }
+
+        // Draw the area selected.
+        private void DrawSelectionBox(Point end_point)
+        {
+            // Save the end point.
+            EndPoint = end_point;
+            if (EndPoint.X < 0) EndPoint.X = 0;
+            if (EndPoint.X >= CroppedImage.Width) EndPoint.X = CroppedImage.Width - 1;
+            if (EndPoint.Y < 0) EndPoint.Y = 0;
+            if (EndPoint.Y >= CroppedImage.Height) EndPoint.Y = CroppedImage.Height - 1;
+
+            // Reset the image.
+            DisplayGraphics.DrawImageUnscaled(CroppedImage, 0, 0);
+
+            // Draw the selection area.
+            int x = Math.Min(StartPoint.X, EndPoint.X);
+            int y = Math.Min(StartPoint.Y, EndPoint.Y);
+            int width = Math.Abs(StartPoint.X - EndPoint.X);
+            int height = Math.Abs(StartPoint.Y - EndPoint.Y);
+            DisplayGraphics.DrawRectangle(Pens.Red, x, y, width, height);
+            picCropped.Refresh();
+        }
+
+        // Display the original image.
+        private void mnuPictureReset_Click(object sender, EventArgs e)
+        {
+            CroppedImage = OriginalImage.Clone() as Bitmap;
+            DisplayImage = OriginalImage.Clone() as Bitmap;
+            DisplayGraphics = Graphics.FromImage(DisplayImage);
+            picCropped.Image = DisplayImage;
+        }
+
+        // Save the current file.
+        private void mnuFileSave_Click(object sender, EventArgs e)
+        {
+            if (sfdPicture.ShowDialog() == DialogResult.OK)
+            {
+                SaveBitmapUsingExtension(CroppedImage, sfdPicture.FileName);
+            }
+        }
+
+        // Save the file with the appropriate format.
+        // Throw a NotSupportedException if the file
+        // has an unknown extension.
+        public void SaveBitmapUsingExtension(Bitmap bm, string filename)
+        {
+            string extension = Path.GetExtension(filename);
+            switch (extension.ToLower())
+            {
+                case ".bmp":
+                    bm.Save(filename, ImageFormat.Bmp);
+                    break;
+                case ".exif":
+                    bm.Save(filename, ImageFormat.Exif);
+                    break;
+                case ".gif":
+                    bm.Save(filename, ImageFormat.Gif);
+                    break;
+                case ".jpg":
+                case ".jpeg":
+                    bm.Save(filename, ImageFormat.Jpeg);
+                    break;
+                case ".png":
+                    bm.Save(filename, ImageFormat.Png);
+                    break;
+                case ".tif":
+                case ".tiff":
+                    bm.Save(filename, ImageFormat.Tiff);
+                    break;
+                default:
+                    throw new NotSupportedException(
+                        "Unknown file extension " + extension);
+            }
+        }
+
+        // Load the image into a Bitmap, clone it, and
+        // set the PictureBox's Image property to the Bitmap.
+        private Bitmap LoadBitmapUnlocked(string file_name)
+        {
+            using (Bitmap bm = new Bitmap(file_name))
+            {
+                Bitmap new_bitmap = new Bitmap(bm.Width, bm.Height);
+                using (Graphics gr = Graphics.FromImage(new_bitmap))
+                {
+                    gr.DrawImage(bm, 0, 0);
+                }
+                return new_bitmap;
+            }
+        }
+    }
+}
